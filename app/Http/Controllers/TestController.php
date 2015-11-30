69d19ab46c2bd7379d3eb1e358bfb624434f2e85
Miroslav Zografski <mzografski@gmail.com>
2015-11-30 10:15:49 +0200

    Test Controller for testing connectivity with additional databases

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,15 +6,51 @@ use App\Http\Requests\CreateSystemRequest;
 use App\System;
 use Illuminate\Http\Request;
 
+use PDO;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
     public function index(){
-    	//here we could test the dynamic connection with other databases 
-    	//$systems = System::all();    	
-    	//foreach ($systems as $system) {} 
-    	return '<p>loop through the systems<br>get the data <br> populate it in the MySQL database<br>perform compare of the states and the values with their limits<br>return json of what shuold be updated in the view.</p>'; 
+    	//test the dynamic connection with other databases seems successfull
+    	$systems = System::all();
+    	$res = [];
+    	foreach ($systems as $system) {
+    		$query = "SELECT [meas].param_id, [meas].time, [meas].value, [param].param_name, [param].param_unit, [limit].ad_limit, [limit].ad_day_limit  FROM [meas] LEFT JOIN [param] on meas.param_id = [param].param_id LEFT JOIN [limit] on meas.param_id = [limit].param_id";
+
+			if($system['host'] == 'WIN-ESA6FH2FC4R\WINCC2K'){
+
+				$port = '1434';
+				$connection = odbc_connect("Driver={SQL Server Native Client 10.0};Server=".$system['host'].",".$port.";Database=".$system['dbname'].";",'cos','web.auto');
+				$results = odbc_exec($connection, $query);
+				
+				$realData = [];
+				$i=0;
+				
+				while ($row = odbc_fetch_object($results)) {
+					$realData[$i] = $row;
+					$i++;
+				}
+
+				$res[$system['name']] = $realData;
+
+				odbc_free_result($results);
+				odbc_close($connection);
+			} else {
+				try{
+    				$conn = new PDO("sqlsrv:Server=".$system['host'].";Database=".$system['dbname'], 'cos', 'web.auto');
+    				$sql = $conn->prepare($query);
+	    			$sql->execute();
+	    			
+	    			$res[$system['name']] = $sql->fetchAll();
+	    			$conn=null;
+    			} catch(PDOExeption $e){
+	    			return "Error: ".$e;
+	    		}
+			}
+    	}    
+    	return $res;
     }
 }
